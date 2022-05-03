b69df0d40fabaf93512d7c6237e8e1df513bcec5
Vivek ganesh <vivek.ganesh09@gmail.com>
2022-05-03 12:54:39 -0400

    Added some comments and tests

--- assets/js/lightmodecontroller.js
+++ assets/js/lightmodecontroller.js
@@ -9,14 +9,22 @@ window.setInterval(function () {
   var date = new Date(); // Create a Date object to find out what time it is
 
   //Calling the function to switch the linked in icon
-  iconSwitch();
+  iconSwitch(); //VG OG commented
 
-  if (date.getHours() >= 11) {
+  //VG  
+  //Calling the function to switch the linked in icon
+  // logoSwitch();
+
+  if (date.getHours() >= 18) {
     document.body.classList.remove("lightMode");
     document.body.classList.add("darkMode");
-  } else if (date.getHours() < 11) {
+    // iconSwitch(); //VG
+    // logoSwitch();//VG
+  } else if (date.getHours() < 18) {
     document.body.classList.remove("darkMode");
     document.body.classList.add("lightMode");
+    // iconSwitch(); //VG
+    // logoSwitch();//VG
   }
 }, 0);
 
@@ -24,8 +32,8 @@ function logoSwitch() {
   if (document.body.classList.contains("lightMode")) {
     siteLogo.src = "assets/neurava.png";
     mobileSiteLogo.src = "assets/neurava.png";
-    logofour.src = "assets/logos/4-light.png";
-    logoseven.src = "assets/logos/7-light.png";
+    logofour.src = "assets/logos/4-dark.png";
+    logoseven.src = "assets/logos/7-dark.png";
   } else {
     mobileSiteLogo.src = "assets/neurava-alt.png";
     siteLogo.src = "assets/neurava-alt.png";
