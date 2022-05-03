9a7d961a3253007cea09e5bfeca0b1ddcaabefd2
Vivek ganesh <vivek.ganesh09@gmail.com>
2022-05-03 12:54:26 -0400

    Added new links and modified news dates
    Added new links and modified news dates

--- json/tool.js
+++ json/tool.js
@@ -1,53 +1,49 @@
 const GLOBAL_NEWS = [
   {
     title:
+      "Neurava awarded Innovation of the Year 2021",
+    date: "FRIDAY, APR 23, 2022",
+    link: "https://techpoint.org/2022/04/neurava-wins-innovation-of-the-year-mira-award/"
+  },
+  {
+    title:
       "Neurava receives a strategic investment from global epilepsy leaders UCB",
-    date: "MONDAY, JAN 24, 2021",
+    date: "FRIDAY, DEC 03, 2021",
     link: "https://www.ucb-usa.com/stories-media/UCB-U-S-News/detail/article/unleashing-power-digital-solutions-epilepsy-care",
   },
   {
     title: "Neurava raises $650,000 in Venture Capital",
-    date: "MONDAY, JAN 24, 2021",
+    date: "MONDAY, NOV 1, 2021",
     link: "https://www.purdue.edu/newsroom/releases/2021/Q4/startup-developing-device-to-prevent-epilepsy-related-death-receives-funding.html",
   },
   {
     title:
       "Neurava wins the Southwest National Pediatric Device Consortium award at RBPC 21",
-    date: "MONDAY, DEC 24, 2021",
-    link: "https://rbpc.rice.edu/Results",
+    date: "FRIDAY, APR 09, 2021",
+    link: "https://houston.innovationmap.com/2021-rice-business-plan-competition-winners-2652291624.html",
   },
   {
     title:
       "Neurava selected into the top 54 startups at the prestigious 2021 Rice Business Plan Competition",
-    date: "MONDAY, DEC 24, 2021",
+    date: "FRIDAY, APR 02, 2021",
     link: "https://rbpc.rice.edu/2021-Competition/2021-startups",
   },
   {
     title: "A wearable device may help prevent unexpected death in epilepsy",
-    date: "MONDAY, DEC 24, 2021",
+    date: "FRIDAY, MAY 12, 2021",
     link: "https://www.purdue.edu/newsroom/releases/2020/Q2/a-wearable-device-may-help-prevent-unexpected-death-in-epilepsy.html",
   },
   {
-    title: "Wearable Device to Prevent Sudden Death from Epilepsy",
-    date: "MONDAY, DEC 24, 2021",
-    link: "https://www.medgadget.com/2020/05/wearable-device-to-prevent-sudden-death-from-epilepsy.html",
-  },
-  {
     title: "Device Could Alert a Potential Deadly Epileptic Event Looms",
-    date: "MONDAY, DEC 24, 2021",
+    date: "WEDNESDAY, SEP 30, 2020",
     link: "https://www.insideindianabusiness.com/story/42703317/device-could-alert-a-potential-deadly-epileptic-event-looms",
   },
   {
-    title: "Wearable Device to Prevent Sudden Death from Epilepsy",
-    date: "MONDAY, DEC 24, 2021",
-    link: "https://www.medgadget.com/2020/05/wearable-device-to-prevent-sudden-death-from-epilepsy.html",
-  },
-  {
     title:
-      "Neurava wins the first annual Elevate Ventures Regional Competition",
-    date: "MONDAY, DEC 24, 2021",
-    link: "https://www.elevateventures.com/elevate-nexus-regional-pitch-competitions-winners-announced/",
-  },
+      "Neurava wins second place at the Purdue University Business Model Competition",
+    date: "THURSDAY, MAY 07, 2020",
+    link: "https://www.insideindianabusiness.com/articles/purdue-student-startups-awarded-dollar100k",
+  }
 ];
 
 const news_div = document.getElementById("newsDiv");
